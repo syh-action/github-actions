32038b7e4a651e849162d759eb2e9af80202407a
syh8088 <syh8088@gmail.com>
2024-10-11 19:35:29 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd 1 test
+          Learn GithubAction cicd 2 test
         </a>
       </header>
     </div>
